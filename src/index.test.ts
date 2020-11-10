5a9cb293ebddd29c8299cb9a359eb6d47796740e
Munif Tanjim <muniftanjim@gmail.com>
2020-11-10 23:36:45 +0600

    feat: initial implementation

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,5 +1,136 @@
-import main from './index'
+import { Pipeline } from './index'
+import type { Middleware } from './index'
 
-test('exports null', () => {
-  expect(main).toBe(null)
+test('invokes initial middlewares', () => {
+  type Context = { value: number }
+
+  const context: Context = { value: 0 }
+
+  const mockFn = jest.fn()
+
+  const plusOne: Middleware<Context> = (ctx, next) => {
+    ctx.value += 1
+    mockFn(ctx.value)
+    next()
+  }
+
+  const pipeline = Pipeline<Context>(plusOne, plusOne)
+
+  pipeline.execute(context)
+
+  expect(context.value).toMatchInlineSnapshot(`2`)
+
+  expect(mockFn).toHaveBeenCalledTimes(2)
+  expect(mockFn.mock.calls).toMatchInlineSnapshot(`
+    Array [
+      Array [
+        1,
+      ],
+      Array [
+        2,
+      ],
+    ]
+  `)
+})
+
+test('invokes pushed middlewares', () => {
+  type Context = { value: number }
+
+  const context: Context = { value: 0 }
+
+  const mockFn = jest.fn()
+
+  const plusOne: Middleware<Context> = (ctx, next) => {
+    ctx.value += 1
+    mockFn(ctx.value)
+    next()
+  }
+
+  const pipeline = Pipeline<Context>(plusOne)
+  pipeline.push(plusOne)
+
+  pipeline.execute(context)
+
+  expect(context.value).toMatchInlineSnapshot(`2`)
+
+  expect(mockFn).toHaveBeenCalledTimes(2)
+  expect(mockFn.mock.calls).toMatchInlineSnapshot(`
+    Array [
+      Array [
+        1,
+      ],
+      Array [
+        2,
+      ],
+    ]
+  `)
+})
+
+test('supports async middlewares', async () => {
+  type Context = { value: number }
+
+  const context: Context = { value: 0 }
+
+  const mockFn = jest.fn()
+
+  const middleware: Middleware<Context> = async (ctx, next) => {
+    await new Promise((resolve) => {
+      setTimeout(() => {
+        ctx.value += 1
+        resolve()
+      })
+    })
+
+    await next()
+  }
+
+  const pipeline = Pipeline<Context>(async (ctx, next) => {
+    mockFn(ctx.value)
+    await next()
+    mockFn(ctx.value)
+  })
+
+  pipeline.push(middleware, middleware)
+
+  await pipeline.execute(context)
+
+  expect(context.value).toMatchInlineSnapshot(`2`)
+
+  expect(mockFn).toHaveBeenCalledTimes(2)
+  expect(mockFn.mock.calls).toMatchInlineSnapshot(`
+    Array [
+      Array [
+        0,
+      ],
+      Array [
+        2,
+      ],
+    ]
+  `)
+})
+
+test('throws on multiple next() calls', async () => {
+  type Context = { value: number }
+
+  const middleware: Middleware<Context> = async (ctx, next) => {
+    await new Promise((resolve) => {
+      setTimeout(() => {
+        ctx.value += 1
+        resolve()
+      })
+    })
+
+    await next()
+    await next()
+  }
+
+  const pipeline = Pipeline<Context>(middleware, middleware)
+
+  const ctx: Context = { value: 0 }
+
+  try {
+    await pipeline.execute(ctx)
+  } catch (err) {
+    expect(err).toMatchInlineSnapshot(`[Error: next() called multiple times]`)
+  }
 })
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,38 @@
-export default null
+type Next = () => Promise<void> | void
+
+export type Middleware<T> = (context: T, next: Next) => Promise<void> | void
+
+type Pipeline<T> = {
+  push: (...middlewares: Middleware<T>[]) => void
+  execute: (context: T) => Promise<void>
+}
+
+export function Pipeline<T>(...middlewares: Middleware<T>[]): Pipeline<T> {
+  const stack: Middleware<T>[] = middlewares
+
+  const push: Pipeline<T>['push'] = (...middlewares) => {
+    stack.push(...middlewares)
+  }
+
+  const execute: Pipeline<T>['execute'] = async (context) => {
+    let prevIndex = -1
+
+    const runner = async (index: number): Promise<void> => {
+      if (index === prevIndex) {
+        throw new Error('next() called multiple times')
+      }
+
+      prevIndex = index
+
+      const middleware = stack[index]
+
+      if (middleware) {
+        await middleware(context, runner.bind(null, index + 1))
+      }
+    }
+
+    await runner(0)
+  }
+
+  return { push, execute }
+}
